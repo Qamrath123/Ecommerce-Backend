9b588048d37abb388c1a67ca1219eb9cfd3023d8
qamrath <qamarathshiek@gmail.com>
2024-08-24 14:19:34 +0530

    added few comments

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,22 +1,16 @@
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-dotenv.config({ path: './config/.env' });
-
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config({ path: "./config/.env" });
 
 const MONGO_URL = process.env.MONGO_URI;
-
-
+//Database connection
 const connect = async () => {
   try {
-
-    
-    await mongoose.connect(MONGO_URL, {
-      
-    });
-    console.log('Database connected successfully');
+    await mongoose.connect(MONGO_URL, {});
+    console.log("Database connected successfully");
   } catch (error) {
-    console.error('Database connection failed:', error.message);
-    process.exit(1); 
+    console.error("Database connection failed:", error.message);
+    process.exit(1);
   }
 };
 
