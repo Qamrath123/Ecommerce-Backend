7819d4bcf3843f1d9d9c5d91c464c31e441067f4
qamrath <qamarathshiek@gmail.com>
2024-08-24 16:44:19 +0530

    Implemented API Endpoints for fetching , updating and deleting product as well

--- controllers/product.js
+++ controllers/product.js
@@ -18,22 +18,99 @@ export const createProd = async (req, res) => {
     return res.status(400).json({ message: "All fields are required." });
   }
   if (typeof price !== "number" || price <= 0) {
-    return res
-    .status(400)
-    .json({ message: "Price must be a positive number" });
+    return res.status(400).json({ message: "Price must be a positive number" });
   }
   try {
     //Creating Product endpoint
 
-    const addedProduct = new ProductModel({ name, description, price, category });
+    const addedProduct = new ProductModel({
+      name,
+      description,
+      price,
+      category,
+    });
     await addedProduct.save();
 
-    res.status(201).json({ message: 'Product created successfully', addedProduct });
+    res
+      .status(201)
+      .json({ message: "Product created successfully", addedProduct });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: 'Server error' });
+    res.status(500).json({ message: "Server error" });
   }
-
-
-  
 };
+
+//Retrieve product
+
+export const getAllProducts = async (req,res) => {
+  try {
+    const products = await ProductModel.find();
+    res.status(201).json({ message: "Fetched Products successfully ", products });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+// Update product 
+
+export const updateProduct = async(req,res) =>{
+
+
+  try{
+    const {id} = req.params;
+    const { name , description , price , category} = req.body;
+
+    // Validate input
+    if (!name || !description || !price || !category) {
+      return res.status(400).json({ message: 'All fields are required' });
+    }
+
+    if (typeof price !== 'number' || price <= 0) {
+      return res.status(400).json({ message: 'Price must be a positive number' });
+    }
+
+//Finding the ID and updating the product
+    const updateProduct = await ProductModel.findByIdAndUpdate(id ,  { name, description, price, category },
+     { new:true}
+    );
+    if (!updateProduct) {
+      return res.status(404).json({ message: 'Product not found' });
+    }
+
+    res.status(200).json({ message: 'Product updated successfully', product: updateProduct });
+
+  }catch(error){
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+
+
+  }
+}
+
+
+//Delete Product
+
+
+export const deleteProduct = async(req,res) =>{
+
+
+
+  try{
+    const { id } = req.params;
+    const deletedProduct = await ProductModel.findByIdAndDelete(id);
+
+    if (!deletedProduct) {
+      return res.status(404).json({ message: 'Product not found' });
+    }
+
+    res.status(200).json({ message: 'Product deleted successfully' });
+
+  }catch(error){
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+
+
+
+  }
+}
--- routes/product.js
+++ routes/product.js
@@ -1,6 +1,12 @@
 import express from 'express';
 import { createProd } from '../controllers/product.js';
+import { getAllProducts } from '../controllers/product.js';
+import { updateProduct } from '../controllers/product.js';
+import { deleteProduct  } from '../controllers/product.js';
 
 const router = express.Router();
 router.post('/add' , createProd);
+router.get('/allProducts' , getAllProducts);
+router.put('/:id', updateProduct);
+router.delete('/:id', deleteProduct)
 export default router;
